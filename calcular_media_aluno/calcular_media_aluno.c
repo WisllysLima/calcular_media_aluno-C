c00003072fa30875fbba89b25985a8c12fec443b
Wisllys Lima <92001619+WisllysLima@users.noreply.github.com>
2022-04-04 20:11:10 -0300

    Add files via upload

---
+++ calcular_media_aluno/calcular_media_aluno.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <locale.h>
+
+
+int main(){
+
+float x, y, z, media;
+
+printf("Digite a nota do primeiro aluno: ");
+scanf("%d", x);
+printf("Digite a segunda nota: ");
+scanf("%d", y);
+printf("Digite a terceira nota: ");
+scanf("%d", z);
+
+media = (x + y + z) / 3;
+
+printf("A media do aluno é: ");
+
+
+}
